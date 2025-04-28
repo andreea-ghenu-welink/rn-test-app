54cad1d7f2f99176cd13d5b8dffd342e8c7ab241
Andreea-Daniela Ghenu <andreea.ghenu@welink.ro>
2025-04-28 14:50:47 +0300

    feat: Add camera launch functionaliy using expo-image-picker

--- app/camera.js
+++ app/camera.js
@@ -1,11 +1,44 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native';
-import { useState } from 'react';
+import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
 
 export default function CameraScreen() {
+  const [cameraPermissionInformation, requestPermission] = useCameraPermissions();
+
+  async function verifyPermissions() {
+    // if the permission is not determined, request permission
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    // if the permission is denied, return false
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert('Insufficient Permissions!', 'You need to grant permission to use the camera');
+      return false;
+    }
+
+    // if the permission is granted, return true
+    return true;
+  }
+
+  async function takeImageHandler() {
+    const hasPermissions = await verifyPermissions();
+
+    if (!hasPermissions) {
+      return;
+    }
+
+    const image = await launchCameraAsync({
+      allowsEditing: true, // allows user to edit the image before saving
+      quality: 0.5, // quality of the image
+    });
+  }
 
   return (
     <View style={styles.container}>
-      <Pressable style={styles.button} onPress={() => {}}>
+      <View>
+      </View>
+      <Pressable style={styles.button} onPress={takeImageHandler}>
         <Text style={styles.buttonText}>Open Camera</Text>
       </Pressable>
     </View>
@@ -33,5 +66,5 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
-  },
+  }
 });
