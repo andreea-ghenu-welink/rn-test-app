fceaf8cc941d6f5f2a548632e75d697ea8f0aac3
Andreea-Daniela Ghenu <andreea.ghenu@welink.ro>
2025-04-28 16:37:32 +0300

    feat: Display image preview

--- app/camera.js
+++ app/camera.js
@@ -1,8 +1,10 @@
-import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { View, Text, Image, Pressable, StyleSheet, Alert } from 'react-native';
+import { useState } from 'react';
 import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
 
 export default function CameraScreen() {
   const [cameraPermissionInformation, requestPermission] = useCameraPermissions();
+  const [pickedImage, setPickedImage] = useState();
 
   async function verifyPermissions() {
     // if the permission is not determined, request permission
@@ -32,15 +34,34 @@ export default function CameraScreen() {
       allowsEditing: true, // allows user to edit the image before saving
       quality: 0.5, // quality of the image
     });
+
+    setPickedImage(image.assets[0].uri);
+  }
+
+  function clearImageHandler() {
+    setPickedImage(null);
+  }
+
+  let imagePreview = <Text style={styles.noImageText}>No image picked yet</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image source={{uri: pickedImage}} style={styles.image} />;
   }
 
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.button} onPress={takeImageHandler}>
+          <Text style={styles.buttonText}>Open Camera</Text>
+        </Pressable>
+
+        {pickedImage && (
+          <Pressable style={[styles.button, styles.clearButton]} onPress={clearImageHandler}>
+            <Text style={styles.buttonText}>Clear Image</Text>
+          </Pressable>
+        )}
       </View>
-      <Pressable style={styles.button} onPress={takeImageHandler}>
-        <Text style={styles.buttonText}>Open Camera</Text>
-      </Pressable>
     </View>
   );
 }
@@ -49,22 +70,53 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    gap: 10,
     backgroundColor: '#fff',
     padding: 20,
   },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    gap: 10,
+    marginTop: 10,
+  },
   button: {
     backgroundColor: '#3498db',
-    width: 170,
     marginHorizontal: 'auto',
     marginTop: 20,
-    padding: 10,
+    width: 170,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
     borderRadius: 5,
+  },
+  clearButton: {
+    backgroundColor: '#A3A5A5',
   },
   buttonText: {
     color: '#fff',
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: 'bold',
     textAlign: 'center',
-  }
+  },
+  imagePreview: {
+    width: "100%",
+    height: 400,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ccc',
+    borderRadius: 8,
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  noImageText: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
 });
